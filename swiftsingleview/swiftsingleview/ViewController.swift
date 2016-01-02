99aa8befebc1d9a085e14c4968c12b35bae68309
htks27 <pr.acc.tks0720@gmail.com>
2016-01-02 13:07:44 +0900

    Add single view application project.

---
+++ swiftsingleview/swiftsingleview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftsingleview
+//
+//  Created by hrkair on 1/2/16.
+//  Copyright © 2016 hrkair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
