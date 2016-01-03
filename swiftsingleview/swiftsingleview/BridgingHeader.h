567c7b9fe3216bf32fb43255a02bc20a13db9293
H.Kinjo <pr.acc.tks0720@gmail.com>
2016-01-03 12:32:53 +0900

    BridgingHeader の設定を追加した。

---
+++ swiftsingleview/swiftsingleview/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  swiftsingleview
+//
+//  Created by hrkair on 1/3/16.
+//  Copyright © 2016 hrkair. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "RESideMenu/RESideMenu.h"
+
+#endif /* BridgingHeader_h */
