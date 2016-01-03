77ccdbbb6925b10fd82a844926f346b489020ba6
H.Kinjo <pr.acc.tks0720@gmail.com>
2016-01-03 12:45:47 +0900

    podfile install 実行結果を反映した。

--- Podfile
+++ Podfile
@@ -1,2 +1,5 @@
 platform :ios, '8.0'
 pod 'RESideMenu', '~> 4.0.7'
+
+xcodeproj 'swiftsingleview/swiftsingleview.xcodeproj'
+
--- swiftsingleview/swiftsingleview/ViewController.swift
+++ swiftsingleview/swiftsingleview/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var menu = RESideMenu();
+        NSLog("%d", menu.contentViewShadowEnabled)
     }
 
     override func didReceiveMemoryWarning() {
